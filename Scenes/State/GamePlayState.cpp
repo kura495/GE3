dcd347b4aec77f401b4c8db8e748e3a3866dce8c
kura495 <104417866+kura495@users.noreply.github.com>
2023-10-03 12:42:40 +0900

    add File Paticle

--- Scenes/State/GamePlayState.cpp
+++ Scenes/State/GamePlayState.cpp
@@ -49,7 +49,7 @@ else {
 	ImGui::SliderFloat3("transform", &viewProjection_.translation_.x, 10.0f, -10.0f);
 	ImGui::SliderFloat3("rotation", &viewProjection_.rotation_.x, 10.0f, -10.0f);
 	ImGui::End();
-
+	light_->ImGui("Light");
 	viewProjection_.UpdateMatrix();
 	
 	player->Update();
