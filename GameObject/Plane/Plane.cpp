1385411838e66cc4240a77e1802453358697c335
kura495 <104417866+kura495@users.noreply.github.com>
2023-10-25 12:59:48 +0900

    add BaseChara and BoxCollider to Plane

--- GameObject/Plane/Plane.cpp
+++ GameObject/Plane/Plane.cpp
@@ -1,1 +1,20 @@
 #include "Plane.h"
+
+Plane::Plane(){}
+Plane::~Plane(){}
+
+void Plane::Initalize(const std::vector<Model*>& models)
+{
+
+}
+
+void Plane::Update()
+{
+
+}
+
+void Plane::Draw()
+{
+
+}
+
--- GameObject/Plane/Plane.h
+++ GameObject/Plane/Plane.h
@@ -1,19 +1,23 @@
 #pragma once
 
-class Plane
+#include "Base/Model.h"
+#include "Base/WorldTransform.h"
+#include "Base/ViewProjection.h"
+
+#include "GameObject/BaseCharacter/BaseCharacter.h"
+#include "Base/Utility/BoxCollider.h"
+
+class Plane : public BaseCharacter,public BoxCollider
 {
 public:
 	Plane();
 	~Plane();
 
+	void Initalize(const std::vector<Model*>& models);
+	void Update();
+	void Draw();
+
 private:
 
 };
 
-Plane::Plane()
-{
-}
-
-Plane::~Plane()
-{
-}
