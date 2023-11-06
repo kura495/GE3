855e2e0ff2f8e6a6e264b7a46080957ae59916f7
kura495 <104417866+kura495@users.noreply.github.com>
2023-11-07 00:34:32 +0900

    add File OBB

---
+++ Base/Utility/OBBoxCollider.cpp
@@ -0,0 +1,11 @@
+#include "OBBoxCollider.h"
+
+void OBBoxCollider::Initalize()
+{
+	
+}
+
+void OBBoxCollider::Update()
+{
+
+}
--- Base/Utility/OBBoxCollider.h
+++ Base/Utility/OBBoxCollider.h
@@ -2,6 +2,12 @@
 
 #include "Collider.h"
 
+struct OBB {
+	Vector3 center;//中心点の座標
+	Vector3 orientations[3];//XYZの各座標軸の傾きを表す方向ベクトル
+	Vector3 size;//中心から面までの距離
+};
+
 class OBBoxCollider : public Collider
 {
 public:
@@ -10,12 +16,5 @@ public:
 
 private:
 
+
 };
-
-OBBoxCollider::OBBoxCollider()
-{
-}
-
-OBBoxCollider::~OBBoxCollider()
-{
-}
