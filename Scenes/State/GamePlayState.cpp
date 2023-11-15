113d8dc1538e4edc8f9775a0d006dd80ca821bbf
kura495 <104417866+kura495@users.noreply.github.com>
2023-11-15 20:57:30 +0900

    Update

--- Scenes/State/GamePlayState.cpp
+++ Scenes/State/GamePlayState.cpp
@@ -130,9 +130,11 @@ else {
 #ifdef _DEBUG
 	ImGui::Begin("MT4_01_01");
 	ImGui::InputFloat4("Matrix0",&rotateMatrix.m[0][0]);
-	ImGui::InputFloat4("Matrix1",&rotateMatrix.m[1][0]);
-	ImGui::InputFloat4("Matrix2",&rotateMatrix.m[2][0]);
-	ImGui::InputFloat4("Matrix3",&rotateMatrix.m[3][0]);
+	ImGui::Text("rotateMatrix");
+	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3]);
+	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3]);
+	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3]);
+	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
 	ImGui::End();
 #endif // _DEBUG
 }
