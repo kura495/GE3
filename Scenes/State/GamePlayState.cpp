ff9616ee5b73864ea199d74b0cc428901d5999c2
kura495 <104417866+kura495@users.noreply.github.com>
2023-11-15 22:38:04 +0900

    Update

--- Scenes/State/GamePlayState.cpp
+++ Scenes/State/GamePlayState.cpp
@@ -131,16 +131,16 @@ else {
 #ifdef _DEBUG
 	ImGui::Begin("MT4_01_04");
 	ImGui::Text("rotation");
-	ImGui::InputFloat4("rotation", &rotation.x);
+	ImGui::Text("%4.2f %4.2f %4.2f %4.2f", rotation.x, rotation.y, rotation.z, rotation.w);
 	ImGui::Text("rotateMatrix");
-	ImGui::InputFloat4("rotateMatrix_0", &rotateMatrix.m[0][0]);
-	ImGui::InputFloat4("rotateMatrix_1", &rotateMatrix.m[1][0]);
-	ImGui::InputFloat4("rotateMatrix_2", &rotateMatrix.m[2][0]);
-	ImGui::InputFloat4("rotateMatrix_3", &rotateMatrix.m[3][0]);
+	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3]);
+	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3]);
+	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3]);
+	ImGui::Text("%4.3f %4.3f %4.3f %4.3f", rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
 	ImGui::Text("rotateByQuaternion");
-	ImGui::InputFloat3("rotateByQuaternion", &rotateByQuaternion.x);
+	ImGui::Text("%4.2f %4.2f %4.2f", rotateByQuaternion.x, rotateByQuaternion.y, rotateByQuaternion.z);
 	ImGui::Text("rotateByMatrix");
-	ImGui::InputFloat3("rotateByMatrix", &rotateByMatrix.x);
+	ImGui::Text("%4.2f %4.2f %4.2f", rotateByMatrix.x, rotateByMatrix.y, rotateByMatrix.z);
 	ImGui::End();
 #endif // _DEBUG
 }
